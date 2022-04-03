5a8a99780c1b18d70253ecdba6494568259274f3
Sephens <adesephens@gmail.com>
2022-04-04 00:39:09 +0300

    Get user input and display in the terminal

--- read.sh
+++ read.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 echo "What is You name"
-
 read name
-
 echo "Hello and welcome $name"
+#wait for some time
+sleep 3
+echo "How old are you $name"
+read age
+sleep 2
+echo "Wow $name You are amazing because  $age is an amazing age!!"
+sleep 3
